832adf937060fa937e1a42a9f20d40d2e21ffc2a
dareen <dareen.alhiyari@gmail.com>
2015-06-14 16:12:25 +0400

    index to return all features based on user segmentation

--- flags/views/api.py
+++ flags/views/api.py
@@ -24,62 +24,15 @@ class APIView(BottleView):
     def index(self, application):
         response.headers["Content-Type"] = "application/json"
         with self.adapter_type() as adapter:
-            return json.dumps(adapter.get_all_keys(application,
-                                                   settings.FEATURES_KEY))
+            application_features = adapter.get_all_keys(application,
+                                                        settings.FEATURES_KEY)
+        user_features = dict()
+        for feature in application_features:
+            user_features[feature] = self._parse_key(application, feature)
+        return user_features
 
     def get(self, application, key):
-        def read_from_adapter(application, key):
-            try:
-                with self.adapter_type() as adapter:
-                    return adapter.read_feature(application, key)
-            except KeyDoesNotExistError:
-                raise HTTPResponse(body="Key does not exist!",
-                                   status=NOT_FOUND)
-
-        def parse_segmentation(segmentation):
-
-            # this flag is segmented
-            # if any of the matching segments is different than the default
-            # value, we return this segment flag
-            for key in request.GET.keys():
-                segment = segmentation.get(key, None)
-
-                if segment:
-                    if segment["enabled"] != settings.DEFAULT_VALUE:
-                        # this segment is enabled/disabled for all
-                        return segment["enabled"]
-                    else:
-                        # not enabled/disabled for all, check user specific
-                        # value
-                        segment_options = segment["options"]
-                        segmented_value = segment_options.get(
-                            request.GET[key],
-                            settings.DEFAULT_VALUE
-                        )
-                        # if it's disabled, return immediately, no need to
-                        # parse the rest of the segments, one disabled segment
-                        # is enough to disable it fot that user
-                        if segmented_value != settings.DEFAULT_VALUE:
-                            return segmented_value
-
-            # the user-specific segment is not available in this application
-            return settings.DEFAULT_VALUE
-
-        value = read_from_adapter(application, key)
-        # If DEFAULT_VALUE is True, then features are Enabled unless stated
-        # otherwise
-        # If DEFAULT_VALUE is False, then features are Disabled unless stated
-        # otherwise
-        if value["enabled"] != settings.DEFAULT_VALUE:
-            # the feature itself is disabled (if DEFAULT_VALUE is True) so no
-            # need to look at the segmentation
-            return value["enabled"]
-        else:
-            # check segmentation
-            segmentation = value["segmentation"]
-
-            user_flag = parse_segmentation(segmentation)
-            return {key: user_flag}
+        return {key: self._parse_key(application, key)}
 
     def post(self, application, key):
         if settings.ADMIN_MODE:
@@ -130,3 +83,62 @@ class APIView(BottleView):
                                     status=NOT_FOUND)
         else:
             return HTTPResponse(status=UNAUTHORIZED)
+
+    def _parse_key(self, application, key):
+        """ read an application key/feature and parse the result based on the
+        user segmentation passed in the request GET params
+        """
+
+        def read_from_adapter(application, key):
+            try:
+                with self.adapter_type() as adapter:
+                    return adapter.read_feature(application, key)
+            except KeyDoesNotExistError:
+                raise HTTPResponse(body="Key does not exist!",
+                                   status=NOT_FOUND)
+
+        def parse_segmentation(segmentation):
+
+            # TODO: this can be refactored to be dynamic
+            # TODO: segmentation should be case-insensitive
+
+            # this flag is segmented
+            # if any of the matching segments is different than the default
+            # value, we return this segment flag
+            for key in request.GET.keys():
+                segment = segmentation.get(key, None)
+
+                if segment:
+                    if segment["enabled"] != settings.DEFAULT_VALUE:
+                        # this segment is enabled/disabled for all
+                        return segment["enabled"]
+                    else:
+                        # not enabled/disabled for all, check user specific
+                        # value
+                        segment_options = segment["options"]
+                        segmented_value = segment_options.get(
+                            request.GET[key],
+                            settings.DEFAULT_VALUE
+                        )
+                        # if it's disabled, return immediately, no need to
+                        # parse the rest of the segments, one disabled segment
+                        # is enough to disable it fot that user
+                        if segmented_value != settings.DEFAULT_VALUE:
+                            return segmented_value
+
+            # the user-specific segment is not available in this application
+            return settings.DEFAULT_VALUE
+
+        value = read_from_adapter(application, key)
+        # If DEFAULT_VALUE is True, then features are Enabled unless stated
+        # otherwise
+        # If DEFAULT_VALUE is False, then features are Disabled unless stated
+        # otherwise
+        if value["enabled"] != settings.DEFAULT_VALUE:
+            # the feature itself is disabled (if DEFAULT_VALUE is True) so no
+            # need to look at the segmentation
+            return value["enabled"]
+        else:
+            # check segmentation
+            segmentation = value["segmentation"]
+            return parse_segmentation(segmentation)
